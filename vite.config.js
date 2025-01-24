4a917cbd74e49a451b5de97d2f2f6baa7f88224e
aambernath <vivek@digambernath.org>
2025-01-24 16:58:47 +0400

    changed vite config for prod

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,32 @@
+//import { defineConfig } from 'vite'
+//import vue from '@vitejs/plugin-vue'
+
+//export default defineConfig({
+//  plugins: [vue()],
+//  server: {
+//    proxy: {
+      // Proxy all calls starting with /api/v1 to your remote server
+//      '/api/v1': {
+//        target: 'https://parts.quarkcs.com',
+//        changeOrigin: true
+        // If your server only supports HTTP, use 'http://', but typically you need https
+//      }
+//    }
+//  }
+//})
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
-    proxy: {
-      // Proxy all calls starting with /api/v1 to your remote server
-      '/api/v1': {
-        target: 'https://parts.quarkcs.com',
-        changeOrigin: true
-        // If your server only supports HTTP, use 'http://', but typically you need https
-      }
-    }
+    // proxy: {
+    //   '/api/v1': {
+    //     target: 'https://parts.quarkcs.com',
+    //     changeOrigin: true
+    //   }
+    // }
   }
 })
+
